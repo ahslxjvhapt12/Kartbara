d66d20f98fc970224e0522ed366666f4ca8c4351
ahslxjvhapt12 <likjhee1@gmail.com>
2023-03-17 12:00:19 +0900

    Rotate
    FollowCam
    Rotate

--- Kartbara/Assets/02.Scripts/CapybaraMove.cs
+++ Kartbara/Assets/02.Scripts/CapybaraMove.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class CapybaraMove : MonoBehaviour
 {
-    [SerializeField] private float speed = 10;
+    [SerializeField] private float speed = 5;
     [SerializeField] private float rotateSpeed = 10;
+
     private void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * speed;
+        Rotate();
+    }
+
+    private void Rotate()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("히히 회전");
+            transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
         }
     }
-
-
 }
