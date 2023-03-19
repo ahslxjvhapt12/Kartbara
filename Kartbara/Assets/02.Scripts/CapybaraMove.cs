77651f0d692eac3d5884ae6a155a057932f1fdf5
ahslxjvhapt12 <likjhee1@gmail.com>
2023-03-20 00:20:47 +0900

    Move
    move

--- Kartbara/Assets/02.Scripts/CapybaraMove.cs
+++ Kartbara/Assets/02.Scripts/CapybaraMove.cs
@@ -1,27 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CapybaraMove : MonoBehaviour
 {
     [SerializeField] private float speed = 5;
     [SerializeField] private float rotateSpeed = 10;
 
-    private void Update()
+    private Rigidbody _rigid = null;
+
+    private void Awake()
     {
-        transform.position += transform.forward * Time.deltaTime * speed;
+        _rigid = GetComponent<Rigidbody>();
+        Debug.Log(Mathf.Repeat(10,3));
+    }
+
+    private void FixedUpdate()
+    {
+
+        _rigid.velocity = (transform.forward * speed) + Vector3.up * _rigid.velocity.y; //new Vector3(_rigid.velocity.x, _rigid.velocity.y, speed);//-transform.up * speed;
         Rotate();
     }
 
     private void Rotate()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
-        }
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+        //}
+
+        float h = Input.GetAxisRaw("Horizontal");
+        //transform.localEulerAngles += new Vector3(0, h * Time.deltaTime * rotateSpeed, 0);
+
+        //transform.rotation *= Quaternion.Euler(0, h * Time.deltaTime * rotateSpeed, 0);
+        transform.rotation = Quaternion.Euler(0, h * Time.deltaTime * rotateSpeed, 0) * transform.rotation;
     }
 }
